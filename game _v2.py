d03c77c43a398f67696ed4a5882b0d1dd011a419
dgedz <d.gedz@inbox.ru>
2022-02-20 21:12:42 +0300

    y

--- game _v2.py
+++ game _v2.py
@@ -23,6 +23,32 @@ def random_predict(number:int=1) -> int:
             break #выход из цикла
     return(count)
 
-if __name__ == '__main__':
-print(f'Количество попыток: {random_predict()}')
 
+
+   
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+# RUN
+#print(f'Количество попыток: {random_predict()}')
+#score_game(random_predict)
+#if __name__ == '__main__':
+score_game(random_predict)
